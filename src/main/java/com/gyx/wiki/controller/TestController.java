eb86d36dc53995040383881063b0b1b8fb01f06d
gyx <gaoyuxin1208@gmail.com>
2023-07-10 22:26:07 +0100

    2-7 增加hello接口测试test.http

--- src/main/java/com/gyx/wiki/controller/TestController.java
+++ src/main/java/com/gyx/wiki/controller/TestController.java
@@ -1,7 +1,7 @@
 package com.gyx.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +9,9 @@ public class TestController {
     @GetMapping ("/hello")
     public String hello(){
         return "Hello World!";
+    }
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! Post, " + name;
     }
 }
