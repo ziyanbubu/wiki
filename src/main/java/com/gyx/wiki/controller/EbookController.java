babe3058e18b6b231321ffea05f16cab0fb49235
gyx <gaoyuxin1208@gmail.com>
2023-07-11 21:31:50 +0100

    3-7 封装请求参数和返回参数

--- src/main/java/com/gyx/wiki/controller/EbookController.java
+++ src/main/java/com/gyx/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.gyx.wiki.controller;
 
-import com.gyx.wiki.domain.Ebook;
+import com.gyx.wiki.req.EbookReq;
 import com.gyx.wiki.res.CommonResp;
+import com.gyx.wiki.res.EbookResp;
 import com.gyx.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping ("/list1")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
