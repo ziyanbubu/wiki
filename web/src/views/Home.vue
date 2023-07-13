2c675de1037ece008ce722db51635a5eef0a2325
gyx <gaoyuxin1208@gmail.com>
2023-07-13 21:05:14 +0100

    4-7 去掉HelloWorld组件相关代码 制作the-header，the-footer组件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -60,14 +60,4 @@
   </a-layout>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-});
-</script>
