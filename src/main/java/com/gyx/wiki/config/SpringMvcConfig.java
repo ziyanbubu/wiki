87e6358c44d86b5f8bfbfb7e39285c7c823cc2d7
gyx <gaoyuxin1208@gmail.com>
2023-07-16 01:38:42 +0100

    5-8 增加AOP，打印接口耗时,请求参数和返回参数 去掉过滤器，拦截器

--- src/main/java/com/gyx/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/gyx/wiki/config/SpringMvcConfig.java
@@ -1,49 +1,49 @@
-package com.gyx.wiki.config;
-
-//import com.gyx.wiki.interceptor.ActionInterceptor;
-import com.gyx.wiki.interceptor.LoginInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LoginInterceptor loginInterceptor;
-
+//package com.gyx.wiki.config;
+//
+////import com.gyx.wiki.interceptor.ActionInterceptor;
+//import com.gyx.wiki.interceptor.LoginInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
 //    @Resource
-//    ActionInterceptor actionInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns(
+//    LoginInterceptor loginInterceptor;
+//
+////    @Resource
+////    ActionInterceptor actionInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns(
 //                        "/test/**",
-                        "/redis/**",
-                        "/user/login",
-                        "/category/all",
-                        "/ebook/list",
-                        "/doc/all/**",
-                        "/doc/vote/**",
-                        "/doc/find-content/**",
-                        "/ebook-snapshot/**",
-                        "/ebook/upload/avatar",
-                        "/file/**"
-                );
-
-//        registry.addInterceptor(actionInterceptor)
-//                .addPathPatterns(
-//                        "/*/save",
-//                        "/*/delete/**",
-//                        "/*/reset-password");
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/file/**").addResourceLocations("file:D:/file/wiki/");
-    }
-}
+//                        "/redis/**",
+//                        "/user/login",
+//                        "/category/all",
+//                        "/ebook/list",
+//                        "/doc/all/**",
+//                        "/doc/vote/**",
+//                        "/doc/find-content/**",
+//                        "/ebook-snapshot/**",
+//                        "/ebook/upload/avatar",
+//                        "/file/**"
+//                );
+//
+////        registry.addInterceptor(actionInterceptor)
+////                .addPathPatterns(
+////                        "/*/save",
+////                        "/*/delete/**",
+////                        "/*/reset-password");
+//    }
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/file/**").addResourceLocations("file:D:/file/wiki/");
+//    }
+//}
