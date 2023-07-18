f57a3bda2e9754d1d2c711bf0804f56be4fe2a92
gyx <gaoyuxin1208@gmail.com>
2023-07-18 01:02:50 +0100

    6-12 编辑时复制对象，修改表单时，不会影响列表信息

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     'vue/no-unused-components': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'vue/no-unused-vars': 'off'
+    'vue/no-unused-vars': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
   }
 }
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -92,6 +92,7 @@
 import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
 import {message} from "ant-design-vue";
+import {Tool} from "../../../public/util/tool";
 
 export default defineComponent({
   name: 'AdminEbook',
@@ -211,7 +212,7 @@ export default defineComponent({
      */
     const edit = (record: any) => {
       modalVisible.value = true;
-      ebook.value = record
+      ebook.value = Tool.copy(record);
     };
 
     /**
