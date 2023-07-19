c936b217696bb03195bccd2cee2f5fe3bee4cad0
gyx <gaoyuxin1208@gmail.com>
2023-07-19 18:49:57 +0100

    7-6 分类选择改为级联选择组件

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -74,11 +74,11 @@
       <a-form-item label="名称">
         <a-input v-model:value="ebook.name" />
       </a-form-item>
-      <a-form-item label="分类一">
-        <a-input v-model:value="ebook.category1Id" />
-      </a-form-item>
-      <a-form-item label="分类二">
-        <a-input v-model:value="ebook.category2Id" />
+      <a-form-item label="分类">
+        <a-cascader v-model:value='categoryIds'
+                    :field-names="{ label: 'name', value:'id', children: 'children' }"
+                    :options="level1"
+                    placeholder="请选择"/>
       </a-form-item>
       <a-form-item label="描述">
         <a-input v-model:value="ebook.description" type="textarea" />
@@ -93,6 +93,7 @@ import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
 import {message} from "ant-design-vue";
 import {Tool} from "../../../public/util/tool";
+
 
 export default defineComponent({
   name: 'AdminEbook',
@@ -172,6 +173,31 @@ export default defineComponent({
       });
     };
 
+
+
+    const level1 = ref(); // 一级分类树，children属性就是二级分类
+    /**
+     * 查询所有分类
+     *
+     */
+    const handleQueryCategory = () => {
+      loading.value = true;
+      axios.get("/category/all").then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if (data.success) {
+          const categorys = data.content;
+          console.log("原始数组：", categorys);
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+        } else {
+          message.error(data.message);
+        }
+
+      });
+    };
+
     /**
      * 表格点击页码时触发
      */
@@ -184,11 +210,14 @@ export default defineComponent({
     };
 
     //-------- 表单 ---------
-    const ebook = ref({});
+    const ebook = ref();
     const modalVisible = ref(false);
     const modalLoading = ref(false);
+    const categoryIds = ref();
     const handleModalOk = () => {
       modalLoading.value = true;
+      ebook.value.category1Id = categoryIds.value[0];
+      ebook.value.category2Id = categoryIds.value[1];
       axios.post("/ebook/save", ebook.value).then((response) => {
         modalLoading.value = false;
         const data = response.data;
@@ -213,6 +242,7 @@ export default defineComponent({
     const edit = (record: any) => {
       modalVisible.value = true;
       ebook.value = Tool.copy(record);
+      categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
     };
 
     /**
@@ -243,6 +273,7 @@ export default defineComponent({
 
 
     onMounted(() => {
+      handleQueryCategory();
       handleQuery({
         page: 1,
         size: pagination.value.pageSize
@@ -260,6 +291,8 @@ export default defineComponent({
       loading,
       handleTableChange,
       handleQuery,
+      categoryIds,
+      level1,
 
       edit,
       add,
