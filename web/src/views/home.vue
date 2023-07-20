dd2584e7c200b99fb80320cfdfe8fbda5303f3aa
gyx <gaoyuxin1208@gmail.com>
2023-07-20 15:43:15 +0100

    7-7 首页显示分类菜单

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -4,42 +4,21 @@
     <a-menu
         mode="inline"
         :style="{ height: '100%', borderRight: 0 }"
+        @click="handleClick"
     >
-      <a-sub-menu key="sub1">
-        <template #title>
-                <span>
-                  <user-outlined />
-                  subnav 1
-                </span>
+      <a-menu-item key="welcome">
+        <router-link :to="'/'">
+          <HomeOutlined />
+          <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+      <a-sub-menu v-for="item in level1" :key="item.id">
+        <template v-slot:title>
+          <span><BarsOutlined />{{item.name}}</span>
         </template>
-        <a-menu-item key="1">option1</a-menu-item>
-        <a-menu-item key="2">option2</a-menu-item>
-        <a-menu-item key="3">option3</a-menu-item>
-        <a-menu-item key="4">option4</a-menu-item>
-      </a-sub-menu>
-      <a-sub-menu key="sub2">
-        <template #title>
-                <span>
-                  <laptop-outlined />
-                  subnav 2
-                </span>
-        </template>
-        <a-menu-item key="5">option5</a-menu-item>
-        <a-menu-item key="6">option6</a-menu-item>
-        <a-menu-item key="7">option7</a-menu-item>
-        <a-menu-item key="8">option8</a-menu-item>
-      </a-sub-menu>
-      <a-sub-menu key="sub3">
-        <template #title>
-                <span>
-                  <notification-outlined />
-                  subnav 3
-                </span>
-        </template>
-        <a-menu-item key="9">option9</a-menu-item>
-        <a-menu-item key="10">option10</a-menu-item>
-        <a-menu-item key="11">option11</a-menu-item>
-        <a-menu-item key="12">option12</a-menu-item>
+        <a-menu-item v-for="child in item.children" :key="child.id">
+          📖 &nbsp; <span>{{child.name}}</span>
+        </a-menu-item>
       </a-sub-menu>
     </a-menu>
   </a-layout-sider>
@@ -81,6 +60,9 @@
 <script lang="ts">
 import {defineComponent, onMounted, ref, reactive, toRef} from "vue";
 import axios from 'axios';
+import _default from "ant-design-vue/es/vc-pagination/Pager";
+import {Tool} from "../../public/util/tool";
+import {message} from "ant-design-vue";
 // const listData: Record<string, string>[] = [];
 //
 // for (let i = 0; i < 23; i++) {
@@ -98,11 +80,42 @@ import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
+  computed: {
+    _default() {
+      return _default
+    }
+  },
   setup(){
     const ebook = ref();
     const ebook1 = reactive({books: []});
 
+    const level1 = ref(); // 一级分类树，children属性就是二级分类
+    let categorys: any;
+    /**
+     * 查询所有分类
+     *
+     */
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组：", categorys);
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const handleClick = () => {
+      console.log("menu click");
+    };
+
     onMounted(() => {
+      handleQueryCategory();
       axios.get("/ebook/list", {
           params: {
             page: 1,
@@ -130,6 +143,8 @@ export default defineComponent({
       ebook,
       pagination,
       actions,
+      handleClick,
+      level1,
     };
 
   }
