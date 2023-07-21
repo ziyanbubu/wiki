7568ca90ba68985b4bcadf995b4348bfc47dfba4
gyx <gaoyuxin1208@gmail.com>
2023-07-21 20:46:06 +0100

    8-6 删除某个文档时，增加二次确认 修复BUG：清空数组，否则多次删除时，数组会一直增

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -86,11 +86,13 @@
 
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue';
+import {createVNode, defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
-import {message} from "ant-design-vue";
+import {message, Modal} from "ant-design-vue";
 import {Tool} from "../../../public/util/tool";
 import {useRoute} from "vue-router";
+import ExclamationCircleOutlined from "@ant-design/icons-vue/ExclamationCircleOutlined";
+
 
 export default defineComponent({
   name: 'AdminDoc',
@@ -224,7 +226,9 @@ export default defineComponent({
       }
     };
 
-    const ids: Array<string> = [];
+    const deleteIds: Array<string> = [];
+    const deleteNames: Array<string> = [];
+
     /**
      * 查找整根树枝
      */
@@ -238,7 +242,9 @@ export default defineComponent({
           console.log("disabled", node);
           // 将目标Id放入结果集ids
           // node.disabled = true;
-          ids.push(id);
+          deleteIds.push(id);
+          deleteNames.push(node.name);
+
 
           // 遍历所有子节点
           const children = node.children;
@@ -296,14 +302,24 @@ export default defineComponent({
      * 删除
      */
     const handleDelete = (id: number) => {
+      // 清空数组，否则多次删除时，数组会一直增加
+      deleteIds.length = 0;
+      deleteNames.length = 0;
       getDeleteIds(level1.value, id);
-      axios.delete("/doc/delete/" + ids.join(",")).then((response) => {
-        const data = response.data;
-        if(data.success) {
-
-          //重新加载列表
-          handleQuery();
-        }
+      Modal.confirm({
+        title: '重要提醒',
+        icon: createVNode(ExclamationCircleOutlined),
+        content: '将删除：【' + deleteNames.join("，") + "】删除后不可恢复，确认删除？",
+        onOk() {
+          // console.log(ids)
+          axios.delete("/doc/delete/" + deleteIds.join(",")).then((response) => {
+            const data = response.data; // data = commonResp
+            if (data.success) {
+              // 重新加载列表
+              handleQuery();
+            }
+          });
+        },
       });
     };
 
