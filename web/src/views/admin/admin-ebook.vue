91fd07479cb6a434ddca8e32be33ba1af4060aca
gyx <gaoyuxin1208@gmail.com>
2023-07-21 19:22:57 +0100

    8-5 增加页面参数传递完成新增文档功能

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -43,8 +43,7 @@
         <template v-slot:action="{ text, record }">
           <a-space size="small">
 
-
-            <router-link to="/admin/doc">
+            <router-link :to="'/admin/doc?ebookId=' + record.id">
               <a-button type="primary" @click="edit(record)">
                 <FileTextOutlined />
               </a-button>
