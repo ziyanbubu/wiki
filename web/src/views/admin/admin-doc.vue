65ae235fa90a93d082ed273ce755b080cf130eab
gyx <gaoyuxin1208@gmail.com>
2023-07-21 21:40:50 +0100

    8-7 集成富文本框插件wangEditor@4.6.3，在文档表单中显示富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -80,6 +80,10 @@
         <a-input v-model:value="doc.sort" />
       </a-form-item>
 
+      <a-form-item label="内容">
+        <div id="content"></div>
+      </a-form-item>
+
     </a-form>
   </a-modal>
 </template>
@@ -92,7 +96,7 @@ import {message, Modal} from "ant-design-vue";
 import {Tool} from "../../../public/util/tool";
 import {useRoute} from "vue-router";
 import ExclamationCircleOutlined from "@ant-design/icons-vue/ExclamationCircleOutlined";
-
+import E from 'wangeditor';
 
 export default defineComponent({
   name: 'AdminDoc',
@@ -178,6 +182,9 @@ export default defineComponent({
     const doc = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
+
+    const editor = new E('#content');
+
     const handleModalOk = () => {
       modalLoading.value = true;
       axios.post("/doc/save", doc.value).then((response) => {
@@ -278,7 +285,9 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
-
+      setTimeout(function () {
+        editor.create();
+      }, 100);
     };
 
     /**
@@ -294,7 +303,9 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
-
+      setTimeout(function () {
+        editor.create();
+      }, 100);
     };
 
 
