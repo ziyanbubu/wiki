f3b3176397f6583ad23e667ede30ceb16f9a76f2
gyx <gaoyuxin1208@gmail.com>
2023-07-23 00:19:11 +0100

    9-4 修改的时候，用户名不允许修改

--- src/main/java/com/gyx/wiki/service/UserService.java
+++ src/main/java/com/gyx/wiki/service/UserService.java
@@ -83,7 +83,9 @@ public class UserService {
             }
         } else {
             // 更新
-            userMapper.updateByPrimaryKey(user);
+            user.setLoginName(null);
+            userMapper.updateByPrimaryKeySelective(user);
+
         }
     }
 
