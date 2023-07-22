3af13125d791b99a875b8d9aafbccdfbb5387653
gyx <gaoyuxin1208@gmail.com>
2023-07-22 23:43:48 +0100

    8-12 BUG修复，初始打开文档管理页面，直接新增文档时，会报错：电子书不能为空

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -191,7 +191,9 @@ export default defineComponent({
     //-------- 表单 ---------
     treeSelectData.value = [];
     const doc = ref();
-    doc.value = {};
+    doc.value = {
+      ebookId: route.query.ebookId
+    };
     const modalVisible = ref(false);
     const modalLoading = ref(false);
 
