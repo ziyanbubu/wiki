9cdd2549c2d461c3378b7a2d7c8578fc2788aa9f
gyx <gaoyuxin1208@gmail.com>
2023-07-23 17:33:58 +0100

    10-2 文档页面显示文档阅读数点赞数

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -16,6 +16,14 @@
           </a-tree>
         </a-col>
         <a-col :span="18">
+          <div>
+            <h2>{{doc.name}}</h2>
+            <div>
+              <span>阅读数：{{doc.viewCount}}</span> &nbsp; &nbsp;
+              <span>点赞数：{{doc.voteCount}}</span>
+            </div>
+            <a-divider style="height: 2px; background-color: #9999cc"/>
+          </div>
           <div class="wangeditor" :innerHTML="html"></div>
         </a-col>
       </a-row>
@@ -38,6 +46,9 @@ export default defineComponent({
     const html = ref();
     const defaultSelectedKeys = ref();
     defaultSelectedKeys.value = [];
+    // 当前选中的文档
+    const doc = ref();
+    doc.value = {};
     /**
      * 一级文档树，children属性就是二级文档
      * [{
@@ -80,6 +91,8 @@ export default defineComponent({
         }            if (Tool.isNotEmpty(level1)) {
           defaultSelectedKeys.value = [level1.value[0].id];
           handleQueryContent(level1.value[0].id);
+          // 初始显示文档信息
+          doc.value = level1.value[0];
         } else {
           message.error(data.message);
         }
@@ -89,6 +102,8 @@ export default defineComponent({
     const onSelect = (selectedKeys: any, info: any) => {
       console.log('selected', selectedKeys, info);
       if (Tool.isNotEmpty(selectedKeys)) {
+        // 选中某一节点时，加载该节点的文档信息
+        doc.value = info.selectedNodes[0].props;
         // 加载内容
         handleQueryContent(selectedKeys[0]);
       }
@@ -102,7 +117,8 @@ export default defineComponent({
       level1,
       html,
       onSelect,
-      defaultSelectedKeys
+      defaultSelectedKeys,
+      doc
     }
   }
 });
