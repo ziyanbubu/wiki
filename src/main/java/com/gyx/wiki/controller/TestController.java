6e6cdd5ab0b319437e06386b9a2f4a8b15d6e961
gyx <gaoyuxin1208@gmail.com>
2023-07-23 16:37:34 +0100

    9-9 增加redis测试接口

--- src/main/java/com/gyx/wiki/controller/TestController.java
+++ src/main/java/com/gyx/wiki/controller/TestController.java
@@ -2,22 +2,29 @@ package com.gyx.wiki.controller;
 
 import com.gyx.wiki.domain.Test;
 import com.gyx.wiki.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
 
     @Value("${test.hello: TEST}")
     private String testHello;
 
     @Resource
     private TestService testService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @GetMapping ("/hello")
     public String hello(){
@@ -32,5 +39,19 @@ public class TestController {
     public List<Test> list(){
 
         return testService.list();
+    }
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
     }
 }
