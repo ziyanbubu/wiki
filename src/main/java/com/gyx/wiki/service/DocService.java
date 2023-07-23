6a00226034f62ce20bb3e5d0763667376b4c062a
gyx <gaoyuxin1208@gmail.com>
2023-07-23 17:38:36 +0100

    10-3 增加文档点赞功能

--- src/main/java/com/gyx/wiki/service/DocService.java
+++ src/main/java/com/gyx/wiki/service/DocService.java
@@ -131,4 +131,11 @@ public class DocService {
             return content.getContent();
         }
     }
+
+    /**
+     * 点赞
+     */
+    public void vote(Long id) {
+        docMapperCust.increaseVoteCount(id);
+    }
 }
